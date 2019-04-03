25ccc8f10476677d3deb5056bca8756993503c87
Cristian Raña <cristian.rana@despegar.com>
2019-04-03 01:12:13 -0300

    [API-2] User model added

--- views/views.py
+++ views/views.py
@@ -1,9 +1,17 @@
-from models import Message
+from models.models import Message
+from models.users import RegularUser
 from run import app
 
 
-@app.route('/', methods=['GET'])
+@app.route('/messages', methods=['GET'])
 def get_messages():
     all_messages = Message.query.all()
-    contents = [message.text_content for message in all_messages]
-    return 'Mensajes: ' + ', '.join(contents)
+    contents = [message.text_content() for message in all_messages]
+    return "Mensajes: " + ", ".join(contents)
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    all_users = RegularUser.query.all()
+    emails = [user.email() for user in all_users]
+    return "Usuarios: " + ", ".join(emails)
