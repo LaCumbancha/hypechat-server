cdf111ffce91393c68f14eab9985360ef7d73774
Cristian Raña <cristian.rana@despegar.com>
2019-04-03 21:30:17 -0300

    [API-4] Useless endpoints deleted

--- views/views.py
+++ views/views.py
@@ -1,22 +1,7 @@
 from models.constants import StatusCode
-from models.models import Message
 from models.users import RegularUser
 from run import app
 from flask import request, jsonify
-
-
-@app.route('/messages', methods=['GET'])
-def get_messages():
-    all_messages = Message.query.all()
-    contents = [message.text_content() for message in all_messages]
-    return "Mensajes: " + ", ".join(contents)
-
-
-@app.route('/users', methods=['GET'])
-def get_users():
-    all_users = RegularUser.query.all()
-    emails = [user.email() for user in all_users]
-    return "Usuarios: " + ", ".join(emails)
 
 
 @app.route('/users/new-user', methods=['POST'])
