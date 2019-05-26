5915104b6c048c341efc710f501b1497fda2b158
Cristian Raña <cristian.rana@despegar.com>
2019-05-26 14:37:36 -0300

    [API-28] Authentication moved from body to cookies

--- models/request.py
+++ models/request.py
@@ -16,9 +16,12 @@ class ClientRequest:
     def logging_request(cls, data):
         logging.getLogger(cls.__name__).info({
             "Headers": data.headers.to_list(),
-            "Body": data.get_json(),
+            "Body": data.get_json() if data.is_json else None,
             "Cookies": data.cookies
         })
+
+    def cookies(self):
+        return self.data.cookies
 
     def json_body(self):
         return self.data.get_json()
@@ -41,36 +44,36 @@ class ClientRequest:
 
     def search_users_data(self, searched_username):
         return SearchUsersDTO(
-            username=self.json_body().get("username"),
-            token=self.json_body().get("auth_token"),
+            username=self.cookies().get("username"),
+            token=self.cookies().get("auth_token"),
             searched_username=searched_username
         )
 
     def authentication_data(self):
         return AuthenticationDTO(
-            username=self.json_body().get("username"),
-            token=self.json_body().get("auth_token")
+            username=self.cookies().get("username"),
+            token=self.cookies().get("auth_token")
         )
 
     def inbox_data(self):
         return InboxDTO(
-            username=self.json_body().get("username"),
-            token=self.json_body().get("auth_token"),
+            username=self.cookies().get("username"),
+            token=self.cookies().get("auth_token"),
             chat_id=self.json_body().get("chat_id"),
             text_content=self.json_body().get("text_content"),
         )
 
     def chat_data(self, chat_id):
         return ChatDTO(
-            username=self.json_body().get("username"),
-            token=self.json_body().get("auth_token"),
+            username=self.cookies().get("username"),
+            token=self.cookies().get("auth_token"),
             chat_id=chat_id
         )
 
     def new_team_data(self):
         return NewTeamDTO(
-            username=self.json_body().get("username"),
-            token=self.json_body().get("auth_token"),
+            username=self.cookies().get("username"),
+            token=self.cookies().get("auth_token"),
             team_name=self.json_body().get("team_name"),
             location=self.json_body().get("location"),
             description=self.json_body().get("description"),
@@ -79,16 +82,16 @@ class ClientRequest:
 
     def invite_data(self, team_id):
         return TeamInviteDTO(
-            username=self.json_body().get("username"),
-            token=self.json_body().get("auth_token"),
+            username=self.cookies().get("username"),
+            token=self.cookies().get("auth_token"),
             team_id=team_id,
             email=self.json_body().get("email")
         )
 
     def accept_invite(self, team_id):
         return TeamInviteAcceptDTO(
-            username=self.json_body().get("username"),
-            token=self.json_body().get("auth_token"),
+            username=self.cookies().get("username"),
+            token=self.cookies().get("auth_token"),
             team_id=team_id,
             invite_token=self.json_body().get("invite_token")
         )
