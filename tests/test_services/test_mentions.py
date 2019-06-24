592b0231f0b67535e01aca26e72843c4247d9657
Cristian Raña <cristian.rana@despegar.com>
2019-06-24 17:36:47 -0300

    [API-13] Mentions tests upgraded to 100%

--- tests/test_services/test_mentions.py
+++ tests/test_services/test_mentions.py
@@ -10,7 +10,7 @@ sys.modules["services.bots"] = MagicMock()
 sys.modules["services.notifications"] = MagicMock()
 sys.modules["logging"].getLogger = MagicMock()
 
-from dtos.models.messages import UserMention, ChannelMention
+from dtos.models.messages import UserMention, ChannelMention, BotMention
 from models.constants import SendMessageType
 from services.mentions import MentionService
 
@@ -50,6 +50,29 @@ class MentionServiceTestCase(unittest.TestCase):
             MockedMentionsDatabase.saved_mentions = MockedMentionsDatabase.batch_mentions
             MockedMentionsDatabase.batch_mentions = []
 
+        sys.modules["daos.messages"].MessageDatabaseClient.add_mention = MagicMock(side_effect=add_mention)
+        sys.modules["daos.database"].DatabaseClient.commit = MagicMock(side_effect=commit)
+
+        MentionService.save_mentions(message, mentions)
+        self.assertEqual(len(mentions), len(MockedMentionsDatabase.saved_mentions))
+        self.assertEqual(0, len(MockedMentionsDatabase.batch_mentions))
+
+    def test_saving_mentions_for_direct_channel_to_regular_user_works_properly(self):
+        message = MagicMock()
+        message.message_id = 0
+        message.send_type = SendMessageType.DIRECT.value
+        mentions = [0]
+
+        def add_mention(mention):
+            from tests.test_services import test_facebook
+            MockedMentionsDatabase.batch_mentions += [mention]
+
+        def commit():
+            from tests.test_services import test_facebook
+            MockedMentionsDatabase.saved_mentions = MockedMentionsDatabase.batch_mentions
+            MockedMentionsDatabase.batch_mentions = []
+
+        sys.modules["daos.bots"].BotDatabaseClient.get_bot_by_id.return_value = None
         sys.modules["daos.messages"].MessageDatabaseClient.add_mention = MagicMock(side_effect=add_mention)
         sys.modules["daos.database"].DatabaseClient.commit = MagicMock(side_effect=commit)
 
@@ -102,6 +125,16 @@ class MentionServiceTestCase(unittest.TestCase):
         self.assertIsNotNone(output_mentions[0].get("username"))
         self.assertIsNotNone(output_mentions[0].get("first_name"))
         self.assertIsNotNone(output_mentions[0].get("last_name"))
+
+    def test_getting_bot_mentions_works_properly(self):
+        mocked_mentions = [BotMention(bot_id=0, bot_name="TEST")]
+
+        sys.modules["daos.messages"].MessageDatabaseClient.get_mentions_by_message.return_value = mocked_mentions
+
+        output_mentions = MentionService.get_mentions(0)
+        self.assertTrue(len(mocked_mentions), len(output_mentions))
+        self.assertEqual(output_mentions[0].get("type"), "BOT")
+        self.assertIsNotNone(output_mentions[0].get("name"))
 
     def test_getting_mixed_mentions_works_properly(self):
         mocked_mentions = [
